a3df1ce1a7f624fe2bb9fdc360aab88e7243eca2
512447 <512447@escg.ac.uk>
2022-02-27 21:18:57 +0000

    Create Photon Gun

---
+++ Assets/Scripts/PhotonGun.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotonGun : MonoBehaviour
+{
+    public Color[] colours;
+    public Transform cameraPoint;
+    public float photonDistance;
+    int currentColourIndex;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(cameraPoint.position, cameraPoint.forward, out hit, photonDistance))
+            if (hit.transform.tag == "Conductive")
+            {
+                Material m = hit.transform.GetComponent<MeshRenderer>().material;
+                m.color = colours[currentColourIndex];
+                hit.transform.GetComponent<MeshRenderer>().material = m;
+            }
+            
+        }
+        else if (Input.GetButtonDown("Fire2"))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(cameraPoint.position, cameraPoint.forward, out hit, photonDistance))
+            if (hit.transform.tag == "Conductive")
+            {
+                Material m = hit.transform.GetComponent<MeshRenderer>().material;
+                m.color = Color.white;
+                hit.transform.GetComponent<MeshRenderer>().material = m;
+            }
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            if (currentColourIndex == colours.Length - 1)
+            currentColourIndex = 0;
+            else 
+            currentColourIndex++;
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            if (currentColourIndex == 0)
+            currentColourIndex = colours.Length - 1;
+            else 
+            currentColourIndex--;
+        }
+    }
+}
