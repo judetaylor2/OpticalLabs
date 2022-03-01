cd3c2c6c60655df9268ad4f0258e042df2e4b0d1
512447 <512447@escg.ac.uk>
2022-03-01 21:41:13 +0000

    add ability for laser projector to  colour objects

--- Assets/Scripts/LaserProjector.cs
+++ Assets/Scripts/LaserProjector.cs
@@ -6,16 +6,58 @@ public class LaserProjector : MonoBehaviour
 {
     MeshRenderer meshRenderer;
     ParticleSystem laserParticle;
-    ParticleSystem.MainModule laserMainModule;
+    public LayerMask groundMask;
+    public PhotonGun photonGun;
+    
     void Start()
     {
         meshRenderer = GetComponent<MeshRenderer>();
         laserParticle = transform.GetChild(0).GetComponent<ParticleSystem>();   
-        laserMainModule = laserParticle.main; 
     }
 
     void Update()
     {
+        ParticleSystem.MainModule laserMainModule = laserParticle.main; 
         laserMainModule.startColor = meshRenderer.material.color;
+        
+        RaycastHit hit;
+        if (Physics.Raycast(laserParticle.transform.position, laserParticle.transform.forward, out hit, 100, groundMask))
+        {
+            if (hit.transform.childCount > 0)
+            {
+                Debug.DrawLine(laserParticle.transform.position, hit.point);
+                
+                Transform t = hit.transform.GetChild(0);
+                
+                for (int i = 0; i < photonGun.colours.Length; i++)
+                {
+                    if (hit.transform.gameObject.layer == 6 && meshRenderer.material.color == photonGun.colours[i])
+                    {
+                        if (i == 0)
+                        {
+                            t.tag = "Speed";
+                        }
+                        else if (i == 1)
+                        {
+                            t.tag = "Gravity";
+                        }
+                        else if (i == 2)
+                        {
+                            t.tag = "Bounce";
+                        }
+
+                        hit.transform.GetComponent<MeshRenderer>().material.color = photonGun.colours[i];                    
+                    }
+                    else if (meshRenderer.material.color == Color.white)
+                    {
+                        t.tag = "Untagged";
+                        hit.transform.GetComponent<MeshRenderer>().material.color = Color.white;
+                        break;
+                    }
+
+            }
+
+            }
+        }
     }
 }
