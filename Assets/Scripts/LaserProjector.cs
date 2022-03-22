04e01fda50490fc9c4608bcc83d7f43f68efa642
512447 <512447@escg.ac.uk>
2022-03-22 17:26:24 +0000

    Start to fix mirror

--- Assets/Scripts/LaserProjector.cs
+++ Assets/Scripts/LaserProjector.cs
@@ -6,7 +6,7 @@ public class LaserProjector : MonoBehaviour
 {
     public MeshRenderer meshRenderer;
     ParticleSystem laserParticle;
-    public LayerMask ground, movableGround, conductiveGround, conductiveMovableGround, conductiveEffectGround;
+    public LayerMask ground, movableGround, conductiveGround, conductiveMovableGround, conductiveEffectGround, conductive;
     public PhotonGun photonGun;
     public bool isMirror;
     Sensor sensorCollider;
@@ -24,18 +24,18 @@ public class LaserProjector : MonoBehaviour
         laserMainModule.startColor = meshRenderer.material.color;
         
         RaycastHit hit;
-        if (Physics.Raycast(laserParticle.transform.position, laserParticle.transform.forward, out hit, 100, conductiveGround | conductiveMovableGround | conductiveEffectGround))
+        if (Physics.Raycast(laserParticle.transform.position, laserParticle.transform.forward, out hit, 100, conductiveGround | conductiveMovableGround | conductiveEffectGround | conductive))
         {
             Debug.DrawLine(laserParticle.transform.position, hit.point, Color.green);
             
-            Transform t = hit.transform;
+            Transform t = hit.collider.transform;
             
-            if (hit.transform.tag != "Filter")
+            if (hit.collider.transform.tag != "Filter")
             for (int i = 0; i < photonGun.colours.Length; i++)
             {
                 if (meshRenderer.material.color == photonGun.colours[i])
                 {
-                    if (hit.transform.gameObject.layer == 10)
+                    if (hit.collider.transform.gameObject.layer == 10)
                     if (i == 0)
                     {
                         t.tag = "Speed";
@@ -49,14 +49,14 @@ public class LaserProjector : MonoBehaviour
                         t.tag = "Bounce";
                     }
 
-                    hit.transform.GetChild(1).GetComponent<MeshRenderer>().material.color = photonGun.colours[i];                    
+                    hit.collider.transform.GetChild(1).GetComponent<MeshRenderer>().material.color = photonGun.colours[i];                    
                 }
                 else if (meshRenderer.material.color == Color.white)
                 {
-                    if (hit.transform.gameObject.layer == 10)
+                    if (hit.collider.transform.gameObject.layer == 10)
                     t.tag = "Untagged";
                     
-                    hit.transform.GetChild(1).GetComponent<MeshRenderer>().material.color = Color.white;
+                    hit.collider.transform.GetChild(1).GetComponent<MeshRenderer>().material.color = Color.white;
                     break;
                 }
 
@@ -65,17 +65,17 @@ public class LaserProjector : MonoBehaviour
             
             Filter filter = laserParticle.GetComponent<Filter>();
             
-            if (hit.transform.tag == "Mirror")
+            if (hit.collider.transform.tag == "Mirror")
             {
                 //if (filter != null)
                 if (filter.particleList.Count > 0)
                 {
                     if (filter.particleList[0].startColor.a > 64)
                     {
-                        hit.transform.GetComponentInParent<Mirror>().isColliding = true;
+                        hit.collider.transform.GetComponentInParent<Mirror>().isColliding = true;
                         
                     
-                        MeshRenderer m = hit.transform.GetComponent<Mirror>().laserObject.GetComponentInParent<MeshRenderer>();
+                        MeshRenderer m = hit.collider.transform.GetComponent<Mirror>().laserObject.GetComponentInParent<MeshRenderer>();
                         Color32 c = m.material.color;
                         c = filter.particleList[0].startColor;
                         m.material.color = c;
@@ -85,18 +85,18 @@ public class LaserProjector : MonoBehaviour
                 }
                 else
                 {
-                    hit.transform.GetComponentInParent<Mirror>().isColliding = true;
+                    hit.collider.transform.GetComponentInParent<Mirror>().isColliding = true;
                         
-                    MeshRenderer m = hit.transform.GetComponent<Mirror>().laserObject.GetComponentInParent<MeshRenderer>();
+                    MeshRenderer m = hit.collider.transform.GetComponent<Mirror>().laserObject.GetComponentInParent<MeshRenderer>();
                     m.material.color = laserParticle.main.startColor.color;
                
                 }
                 
             }
             
-            if (hit.transform.tag == "Sensor")
+            if (hit.collider.transform.tag == "Sensor")
             {
-                sensorCollider = hit.transform.GetComponentInParent<Sensor>();
+                sensorCollider = hit.collider.transform.GetComponentInParent<Sensor>();
                 sensorCollider.isCollidingWithLaser = true;
                
             }
--- Assets/Scripts/PhotonGun.cs
+++ Assets/Scripts/PhotonGun.cs
@@ -26,34 +26,35 @@ public class PhotonGun : MonoBehaviour
             RaycastHit hit;
             if (Physics.Raycast(raycastStartPoint.position, raycastStartPoint.forward, out hit, photonDistance, Physics.AllLayers))
             {
-                if ((hit.transform.gameObject.layer == 8 || hit.transform.gameObject.layer == 9 || hit.transform.gameObject.layer == 10 || hit.transform.tag == "Filter") && hit.transform.name != "LaserProjector")
+                Debug.Log ("name = " + LayerMask.LayerToName(hit.collider.gameObject.layer));
+                if ((hit.collider.gameObject.layer == 8 || hit.collider.gameObject.layer == 9 || hit.collider.gameObject.layer == 10 || hit.collider.gameObject.layer == 13 || hit.collider.transform.tag == "Filter") && hit.collider.transform.name != "LaserProjector")
                 {
-                    Material m = hit.transform.GetChild(1).GetComponent<MeshRenderer>().material;
+                    Material m = hit.collider.transform.GetChild(1).GetComponent<MeshRenderer>().material;
                     m.color = colours[currentColourIndex];
                     
-                    if (hit.transform.gameObject.layer == 10)
+                    if (hit.collider.gameObject.layer == 10)
                     if (currentColourIndex == 0)
                     {
-                        //speedCollider.transform.localScale = hit.transform.localScale;
-                        //Instantiate(speedCollider, hit.transform.position, hit.transform.rotation);
+                        //speedCollider.transform.localScale = hit.collider.transform.localScale;
+                        //Instantiate(speedCollider, hit.collider.transform.position, hit.collider.transform.rotation);
 
-                        hit.transform.tag = "Speed";
+                        hit.collider.transform.tag = "Speed";
                     }
                     else if (currentColourIndex == 1)
                     {
-                        hit.transform.tag = "Gravity";
+                        hit.collider.transform.tag = "Gravity";
                     }
                     else if (currentColourIndex == 2)
                     {
-                        hit.transform.tag = "Bounce";
+                        hit.collider.transform.tag = "Bounce";
                     }                    
                     
                 }
-                else if (hit.transform.name == "LaserProjector")
+                else if (hit.collider.transform.name == "LaserProjector")
                 {
-                    Material m = hit.transform.GetChild(1).GetComponent<MeshRenderer>().material;
+                    Material m = hit.collider.transform.GetChild(1).GetComponent<MeshRenderer>().material;
                     m.color = colours[currentColourIndex];
-                    hit.transform.GetChild(1).GetComponent<MeshRenderer>().material = m;
+                    hit.collider.transform.GetChild(1).GetComponent<MeshRenderer>().material = m;
                 }
                 
             }
@@ -63,13 +64,13 @@ public class PhotonGun : MonoBehaviour
         {
             RaycastHit hit;
             if (Physics.Raycast(raycastStartPoint.position, raycastStartPoint.forward, out hit, photonDistance))
-            if (hit.transform.gameObject.layer == 8 || hit.transform.gameObject.layer == 9 || hit.transform.gameObject.layer == 10 || hit.transform.tag == "Filter")
+            if (hit.collider.gameObject.layer == 8 || hit.collider.gameObject.layer == 9 || hit.collider.gameObject.layer == 10 || hit.collider.transform.tag == "Filter")
             {
-                Material m = hit.transform.GetChild(1).GetComponent<MeshRenderer>().material;
+                Material m = hit.collider.transform.GetChild(1).GetComponent<MeshRenderer>().material;
                 m.color = Color.white;
-                hit.transform.GetChild(1).GetComponent<MeshRenderer>().material = m;
+                hit.collider.transform.GetChild(1).GetComponent<MeshRenderer>().material = m;
 
-                hit.transform.tag = "Untagged";
+                hit.collider.transform.tag = "Untagged";
             }
         }
         else if (Input.GetAxis("Mouse ScrollWheel") > 0)
